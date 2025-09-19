7e4112d55afb11a3c805dff3641d5147406e9774
Kostiantyn Golosov <kongol.ml@gmail.com>
2025-09-20 00:14:40 +0200

    portoflio

--- src/app/components/PortfolioList/PortfolioList.tsx
+++ src/app/components/PortfolioList/PortfolioList.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { useState, useMemo, useEffect, useRef, useCallback } from "react";
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
+
+import Modal from "react-bootstrap/Modal";
+import CloseButton from "react-bootstrap/CloseButton";
 
 // types
 import { PortfolioCategory, PortfolioCategorySlugs, PortfolioItem } from "@/types/portfolio.types";
+
+import Image from "@/app/components/common/Image";
 
 // styles
 import styles from "./PortfolioList.module.scss";
@@ -30,34 +35,34 @@ const categoriesIconsMap = {
 const useElementWidth = () => {
 	const ref = useRef(null);
 	const [width, setWidth] = useState(0);
-  
+
 	const updateWidth = useCallback(() => {
-	  if (ref.current) {
-		setWidth(ref.current.offsetWidth);
-	  }
+		if (ref.current) {
+			setWidth(ref.current.offsetWidth);
+		}
 	}, []);
-  
+
 	useEffect(() => {
-	  updateWidth(); // Set initial width
-  
-	  const element = ref.current;
-	  if (!element) return;
-  
-	  // Use ResizeObserver to update width when the element resizes
-	  const observer = new ResizeObserver(() => {
-		updateWidth();
-	  });
-  
-	  observer.observe(element);
-  
-	  // Cleanup function to disconnect the observer
-	  return () => {
-		observer.disconnect();
-	  };
+		updateWidth(); // Set initial width
+
+		const element = ref.current;
+		if (!element) return;
+
+		// Use ResizeObserver to update width when the element resizes
+		const observer = new ResizeObserver(() => {
+			updateWidth();
+		});
+
+		observer.observe(element);
+
+		// Cleanup function to disconnect the observer
+		return () => {
+			observer.disconnect();
+		};
 	}, [updateWidth]);
-  
+
 	return { ref, width };
-  };
+};
 
 export default function PortfolioList({ portfolioItems, portfolioCategories }: PortfolioListProps) {
 	const router = useRouter();
@@ -108,15 +113,29 @@ export default function PortfolioList({ portfolioItems, portfolioCategories }: P
 		targetRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
 	};
 
+	const [show, setShow] = useState(false);
+	const [selectedPortfolioItem, setSelectedPortfolioItem] = useState<PortfolioItem | null>(null);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	const showPortfolioItem = (item: PortfolioItem) => {
+		setSelectedPortfolioItem(item);
+		setShow(true);
+	};
+
+	const closePortfolioItem = () => {
+		setSelectedPortfolioItem(null);
+		handleClose();
+	};
+
 	return (
 		<>
 			<div className="container">
-				{/* <div className="row"> */}
-				{/* <PortfolioCategories portfolioCategories={portfolioCategories}  /> */}
-				{/* </div> */}
-
 				<div className="d-flex justify-content-center flex-column vh-100 ">
-					<div className="row"><h1>Projects</h1></div>
+					<div className="row">
+						<h1>Projects</h1>
+					</div>
 
 					<div className="row row-cols-1 row-cols-lg-2 row-cols-xl-3 w-100">
 						{portfolioCategories.map((category, index) => {
@@ -136,54 +155,55 @@ export default function PortfolioList({ portfolioItems, portfolioCategories }: P
 					</div>
 				</div>
 
-				{/* <div className={styles.flexContainer} >
-					{portfolioCategories.map((category, index) => {
-						const CategoryIcon = categoriesIconsMap[category.slug];
-
-						return (
-							<div  key={category.slug} onClick={() => scrollToCategory(category.slug)} className={styles.flexItem} style={{ width: sourceWidth }}>
-								<h2>
-									{category.name} <span className={styles.itemsCount}>{(categoryCounts && categoryCounts[category.slug]) || 0}</span>
-								</h2>
-								<div className={`${styles.portfolioImageWrp} ${styles[category.slug]} d-flex justify-content-center align-items-center`}>
-									<CategoryIcon className={styles.svgIcon} />
+				<Modal show={show} onHide={closePortfolioItem} fullscreen animation={false}>
+					<CloseButton onClick={closePortfolioItem} className={styles.closeButton} />
+					<Modal.Body className="modal-body d-flex flex-column justify-content-center align-items-center text-center">
+						{selectedPortfolioItem && (
+							<div>
+								<div className={styles.portfolioItemWrp}>
+									{/* Fill the 16:9 box. Choose cover (crop) or contain (letterbox). */}
+									<Image
+										src={selectedPortfolioItem.thumbnail}
+										alt={selectedPortfolioItem.title}
+										fill
+										sizes="90vw"
+										style={{ objectFit: "contain" }} /* or "cover" */
+									/>
+									<p className={styles.portfolioItemDescription}>{selectedPortfolioItem.description}</p>
 								</div>
 							</div>
-						);
-					})}
-				</div> */}
+						)}
+					</Modal.Body>
+				</Modal>
 			</div>
 
-			{/* <div className={styles.projectsGrid}> */}
+			<div className="container" ref={targetRef}>
+				<div className="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
+					{filteredItems.map((item) => (
+						<div className="col" key={item.id}>
+							<div onClick={() => showPortfolioItem(item)}>
+								<div className={styles.projectCard}>
+									{item.thumbnail && (
+										<div className={styles.imageWrap}>
+											<Image
+												src={item.thumbnail}
+												alt={item.title}
+												fill
+												className={styles.projectImg}
+												sizes="(min-width: 1200px) 33vw, (min-width: 768px) 33vw, 100vw"
+												draggable={false}
+												onContextMenu={(e) => e.preventDefault()}
+											/>
+										</div>
+									)}
 
-				<div className="container" ref={targetRef}>
-					<div className="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
-						{filteredItems.map((item) => (
-							<div className="col" key={item.id}>
-								<Link href={`/projects/${item.slug}`}>
-									<div className={styles.projectCard}>
-										{item.thumbnail && (
-											<div className={styles.imageWrap}>
-												<Image
-													src={item.thumbnail}
-													alt={item.title}
-													fill
-													className={styles.projectImg}
-													sizes="(min-width: 1200px) 33vw, (min-width: 768px) 33vw, 100vw"
-													draggable={false}
-													onContextMenu={(e) => e.preventDefault()}
-												/>
-											</div>
-										)}
-
-										<p>{item.title}</p>
-									</div>
-								</Link>
+									<p>{item.title}</p>
+								</div>
 							</div>
-						))}
-					</div>
+						</div>
+					))}
 				</div>
-
+			</div>
 		</>
 	);
 }
